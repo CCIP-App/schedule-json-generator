4447c5c963d12fac1485eed0d136087eacab99f3
Denny Huang <denny0223@gmail.com>
2019-02-08 21:02:38 +0800

    Initial commit

---
+++ config-sample.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.spreadsheetKey = "1knHgrDErElEvj0W9GvFm-_uXk-GaXL3kA8sONq-EHv0"
+
+config.default_avatar = "https://sitcon.org/2018/static/img/staffs/stone.png"
+config.avatar_base_url = "https://sitcon.org/2018/static/img/speaker/"
+
+module.exports = config
