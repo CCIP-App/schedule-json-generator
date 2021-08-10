6506ca57465fca3850a94b590551e54ff9773671
腹黒い茶 <haraguroicha@tipsy.coffee>
2021-08-11 00:43:11 +0800

    using harder way to fetch OPass schedule from spreadsheets to json file

--- index.js
+++ index.js
@@ -1,131 +1,150 @@
 const config = require("./config");
-const { extractSheets } = require("spreadsheet-to-json");
+const fetch = require('node-fetch');
+const fetchSettings = { method: "GET" };
 
 function nullCoalesce(value) {
-  return value == null ? "" : value
+  return value == null ? "" : value;
 }
 
-extractSheets(
-  {
-    spreadsheetKey: config.spreadsheetKey
-  },
-  function(err, data) {
-    if (err) console.log(err);
-    data.Session.shift();
-    data.Speaker.shift();
+const getSpreadsheetJSONEndpoint = (spreadSheetId, sheetId) => `https://spreadsheets.google.com/feeds/list/${spreadSheetId}/${sheetId}/public/values?alt=json`;
 
-    data.Session.forEach((session) => {
-      // title & description
-      session.zh = {}
-      session.zh.title = nullCoalesce(session.title_zh)
-      session.zh.description = nullCoalesce(session.description_zh)
+const opassSessionUrl = getSpreadsheetJSONEndpoint(config.spreadsheetKey, 1);
+const opassSpeakerUrl = getSpreadsheetJSONEndpoint(config.spreadsheetKey, 2);
+const opassSessionTypeUrl = getSpreadsheetJSONEndpoint(config.spreadsheetKey, 3);
+const opassRoomUrl = getSpreadsheetJSONEndpoint(config.spreadsheetKey, 4);
+const opassTagUrl = getSpreadsheetJSONEndpoint(config.spreadsheetKey, 5);
 
-      session.en = {}
-      session.en.title = nullCoalesce(session.title_en)
-      session.en.description = nullCoalesce(session.description_en)
+let data = {
+  sessions: {},
+  speakers: {},
+  session_types: {},
+  rooms: {},
+  tags: {},
+};
 
-      delete session.title_zh
-      delete session.description_zh
-      delete session.title_en
-      delete session.description_en
+const fetchError = (e) => console.error(e)
 
-      // speaker
-      session.speakers = []
+const sessionFetch = () => fetch(opassSessionUrl, fetchSettings)
+  .then(res => res.json())
+  .then(json => {
+    s = json.feed.entry;
+    s.shift()
+    data.sessions = s.map(e => {
+      return {
+        id: e['gsx$id']['$t'],
+        zh: {
+          title: nullCoalesce(e['gsx$titlezh']['$t']),
+          description: nullCoalesce(e['gsx$descriptionzh']['$t']),
+        },
+        en: {
+          title: nullCoalesce(e['gsx$titleen']['$t']),
+          description: nullCoalesce(e['gsx$descriptionen']['$t']),
+        },
+        speakers: [...Array(9).keys()].slice(1).map(sp => (e[`gsx$speaker${sp}id`] || {'$t':''})['$t']).filter(f => f),
+        broadcast: e['gsx$broadcast']['$t'].split(',').filter(f => f),
+        tags: [...Array(4).keys()].slice(1).map(sp => (e[`gsx$tag${sp}`] || {'$t':''})['$t']).filter(f => f),
+        type: e['gsx$type']['$t'],
+        room: e['gsx$room']['$t'],
+        start: e['gsx$start']['$t'],
+        end: e['gsx$end']['$t'],
+        qa: e['gsx$qa']['$t'],
+        slide: e['gsx$slide']['$t'],
+        co_write: e['gsx$cowrite']['$t'],
+        live: e['gsx$live']['$t'],
+        record: e['gsx$record']['$t'],
+        language: e['gsx$language']['$t'],
+      };
+    });
+  });
+const speakerFetch = () => fetch(opassSpeakerUrl, fetchSettings)
+  .then(res => res.json())
+  .then(json => {
+    s = json.feed.entry;
+    s.shift()
+    data.speakers = s.map(e => {
+      let avatar = e['gsx$avatar']['$t'];
+      return {
+        id: e['gsx$id']['$t'],
+        avatar: avatar.length == 0 ? config.default_avatar : config.avatar_base_url + avatar,
+        zh: {
+          name: nullCoalesce(e['gsx$namezh']['$t']),
+          bio: nullCoalesce(e['gsx$biozh']['$t']),
+        },
+        en: {
+          name: nullCoalesce(e['gsx$nameen']['$t']),
+          bio: nullCoalesce(e['gsx$bioen']['$t']),
+        },
+      };
+    });
+  });
+const sessionTypeFetch = () => fetch(opassSessionTypeUrl, fetchSettings)
+  .then(res => res.json())
+  .then(json => {
+    data.session_types = json.feed.entry.map(e => {
+      return {
+        id: e['gsx$id']['$t'],
+        zh: {
+          name: nullCoalesce(e['gsx$namezh']['$t']),
+          description: nullCoalesce(e['gsx$descriptionzh']['$t']),
+        },
+        en: {
+          name: nullCoalesce(e['gsx$nameen']['$t']),
+          description: nullCoalesce(e['gsx$descriptionen']['$t']),
+        },
+      };
+    });
+  });
+const roomFetch = () => fetch(opassRoomUrl, fetchSettings)
+  .then(res => res.json())
+  .then(json => {
+    data.rooms = json.feed.entry.map(e => {
+      return {
+        id: e['gsx$id']['$t'],
+        zh: {
+          name: nullCoalesce(e['gsx$namezh']['$t']),
+          description: nullCoalesce(e['gsx$descriptionzh']['$t']),
+        },
+        en: {
+          name: nullCoalesce(e['gsx$nameen']['$t']),
+          description: nullCoalesce(e['gsx$descriptionen']['$t']),
+        },
+      };
+    });
+  });
+const tagFetch = () => fetch(opassTagUrl, fetchSettings)
+  .then(res => res.json())
+  .then(json => {
+    data.tags = json.feed.entry.map(e => {
+      return {
+        id: e['gsx$id']['$t'],
+        zh: {
+          name: nullCoalesce(e['gsx$namezh']['$t']),
+          description: nullCoalesce(e['gsx$descriptionzh']['$t']),
+        },
+        en: {
+          name: nullCoalesce(e['gsx$nameen']['$t']),
+          description: nullCoalesce(e['gsx$descriptionen']['$t']),
+        },
+      };
+    });
+  });
 
-      for (i = 1; i <= 8; i++) {
-        if (session['speaker' + i + 'id'] != null) {
-          session.speakers.push(session['speaker' + i + 'id'])
-        }
+const fetches = [ sessionFetch, speakerFetch, sessionTypeFetch, roomFetch, tagFetch ];
+let fetcheAll = fetches.map(f => f());
 
-        delete session['speaker' + i]
-        delete session['speaker' + i + 'id']
+const doAll = () => Promise.allSettled(fetcheAll)
+  .then(results => {
+    fetcheAll = []
+    for (const [index, result] of results.entries()) {
+      // console.log(index, result.status)
+      if (result.status == 'rejected') {
+        fetcheAll.push(fetches[index]())
       }
-
-      // broadcast
-      if (session.broadcast != null) {
-        session.broadcast = session.broadcast.split(',')
-      }
-
-      // tag
-      session.tags = []
-
-      for (i = 1; i <= 3; i++) {
-        if (session['tag' + i] != null) {
-          session.tags.push(session['tag' + i])
-        }
-
-        delete session['tag' + i]
-      }
-    })
-
-    data.Speaker.forEach((speaker) => {
-      speaker.avatar = speaker.avatar == undefined ? config.default_avatar : config.avatar_base_url + speaker.avatar
-
-      speaker.zh = {}
-      speaker.zh.name = nullCoalesce(speaker.name_zh)
-      speaker.zh.bio = nullCoalesce(speaker.bio_zh)
-
-      speaker.en = {}
-      speaker.en.name = nullCoalesce(speaker.name_en)
-      speaker.en.bio = nullCoalesce(speaker.bio_en)
-
-      delete speaker.name_zh
-      delete speaker.bio_zh
-      delete speaker.name_en
-      delete speaker.bio_en
-    })
-
-    data.SessionType.forEach((type) => {
-      type.zh = {}
-      type.zh.name = nullCoalesce(type.name_zh)
-      type.zh.description = nullCoalesce(type.description_zh)
-
-      type.en = {}
-      type.en.name = nullCoalesce(type.name_en)
-      type.en.description = nullCoalesce(type.description_en)
-
-      delete type.name_zh
-      delete type.name_en
-      delete type.description_zh
-      delete type.description_en
-    })
-
-    data.Room.forEach((room) => {
-      room.zh = {}
-      room.zh.name = nullCoalesce(room.name_zh)
-      room.zh.description = nullCoalesce(room.description_zh)
-
-      room.en = {}
-      room.en.name = nullCoalesce(room.name_en)
-      room.en.description = nullCoalesce(room.description_en)
-
-      delete room.name_zh
-      delete room.name_en
-      delete room.description_zh
-      delete room.description_en
-    })
-
-    data.Tag.forEach((tag) => {
-      tag.zh = {}
-      tag.zh.name = nullCoalesce(tag.name_zh)
-      tag.zh.description = nullCoalesce(tag.description_zh)
-
-      tag.en = {}
-      tag.en.name = nullCoalesce(tag.name_en)
-      tag.en.description = nullCoalesce(tag.description_en)
-
-      delete tag.name_zh
-      delete tag.name_en
-      delete tag.description_zh
-      delete tag.description_en
-    })
-
-    console.log(JSON.stringify({
-      "sessions": data.Session,
-      "speakers": data.Speaker,
-      "session_types": data.SessionType,
-      "rooms": data.Room,
-      "tags": data.Tag,
-    }))
-  }
-)
+    }
+    if (fetcheAll.length > 0) {
+      doAll();
+    } else {
+      console.log(JSON.stringify(data))
+    }
+  })
+doAll()
